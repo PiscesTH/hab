915b130447f8b59cc2e3eb2a6e2fef046c728a9a
TH <kareme007@gmail.com>
2024-08-19 15:33:26 +0900

    2024-08-19

--- src/App.js
+++ src/App.js
@@ -2,14 +2,48 @@ import logo from "./logo.svg";
 import "./App.css";
 import { useState } from "react";
 
+<bill>
+  <div>
+
+  </div>
+</bill>
+
 function App() {
   return (
     <div className="App">
       <body>
         <div className="container">
-          <div className="header">header</div>
+          <div className="header">
+            <div className="header-container">
+              <h1 className="th">
+                <a href="#">TH</a>
+              </h1>
+              <div className="menu">
+                <nav>
+                  <ul className="header-nav">
+                    <li>
+                      <button>
+                        <a href="#">통계</a>
+                      </button>
+                    </li>
+                    <li>
+                      <button>로그인</button>
+                    </li>
+                    <li>
+                      <button>button3</button>
+                    </li>
+                  </ul>
+                </nav>
+              </div>
+            </div>
+          </div>
           <div className="left-side">left-side</div>
-          <div className="main">main</div>
+          <div className="main">
+            <div className="main-inner">
+              <div className="main-inner-left">Laaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</div>
+              <div className="main-inner-right">R</div>
+            </div>
+          </div>
           <div className="footer">footer</div>
           <div className="right-side">right-side</div>
         </div>
