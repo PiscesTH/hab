acabbc4fe2aababf78dedf3105e98981184eae16
TH <kareme007@gmail.com>
2024-08-19 13:50:04 +0900

    hab

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <body>
+        <div className="container">
+          <div className="header">header</div>
+          <div className="left-side">left-side</div>
+          <div className="main">main</div>
+          <div className="footer">footer</div>
+          <div className="right-side">right-side</div>
+        </div>
+        <div className="tmp"></div>
+      </body>
     </div>
   );
 }
