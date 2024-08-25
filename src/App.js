d88e9fda3d10763ada47204fc955ca45ac7c7eae
TH <kareme007@gmail.com>
2024-08-25 17:08:13 +0900

    2024-08-25

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import logo from "./logo.svg";
 import "./App.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import React, { PureComponent } from "react";
 import Chart from "./Chart.js";
 import LineChart from "./LineChart.js";
 import { NavLink, Route, Routes, Link } from "react-router-dom";
 import Calendar from "react-calendar";
-import 'react-calendar/dist/Calendar.css';
+import "react-calendar/dist/Calendar.css";
 import moment from "moment";
 
 function MainPage() {
-  return <div className="main-container">
+  return (
+    <div className="main-container">
       <div className="income">
         <div>
           <p className="a">이번달 수입</p>
@@ -31,22 +32,59 @@ function MainPage() {
         <p>일별 지출</p>
         <LineChart></LineChart>
       </div>
-    </div>;
+    </div>
+  );
 }
 
 function List() {
-  return <div className="list-container">
-      <Calendar 
-                formatDay={(locale, date) => moment(date).format("D")}
-                formatYear={(locale, date) => moment(date).format("YYYY")}
-                formatMonthYear={(locale, date) => moment(date).format("YYYY. MM")}
-                calendarType="gregory"
-                showNeighboringMonth={false}
-                next2Label={null}
-                prev2Label={null} 
-                minDetail="year">
-                </Calendar>
-    </div>;
+  const [date, selectDate] = useState(new Date());
+
+  return (
+    <div className="list-container">
+      <Calendar
+        onChange={selectDate}
+        value={date}
+        formatDay={(locale, date) => moment(date).format("D")}
+        formatYear={(locale, date) => moment(date).format("YYYY")}
+        formatMonthYear={(locale, date) => moment(date).format("YYYY. MM")}
+        calendarType="gregory"
+        showNeighboringMonth={false}
+        next2Label={null}
+        prev2Label={null}
+        minDetail="year"
+      ></Calendar>
+      {/* useEffect(() => {
+        console.log(date)
+      }, [date]); */}
+      <MyForm date={date} />
+      <div className="read">read</div>
+    </div>
+  );
+}
+
+function MyForm(props) {
+  const [date, setDate] = useState(moment(props.date).format("YYYY-MM-DD"));
+
+  const handleDateChange = (event) => {
+    setDate(event.target.value);
+  };
+
+  return (
+    <div className="form-container">
+      <input
+        type="text"
+        className="form-date"
+        value={date}
+        onChange={handleDateChange}
+      ></input>
+      <input type="text" className="form-title" placeholder="Title" />
+      <textarea
+        className="form-textarea"
+        placeholder="Write something..."
+      ></textarea>
+      <button className="form-submit">SUBMIT</button>
+    </div>
+  );
 }
 
 function App() {
@@ -84,8 +122,8 @@ function App() {
           </header>
           <main>
             <Routes>
-              <Route exact path="/" element={<MainPage/>}></Route>
-              <Route path="/list" element={<List/>}></Route>
+              <Route exact path="/" element={<MainPage />}></Route>
+              <Route path="/list" element={<List />}></Route>
             </Routes>
           </main>
           <footer></footer>
